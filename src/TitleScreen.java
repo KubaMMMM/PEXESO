49391add6457c576b5757d9e4895293eb2093ff3
Kuba <janhra155@gmail.com>
2026-04-21 19:25:34 +0200

    prvni okno 2

--- src/TitleScreen.java
+++ src/TitleScreen.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.FileNotFoundException;
 
 public class TitleScreen extends JFrame {
     private JFrame frame;
@@ -24,30 +25,49 @@ public class TitleScreen extends JFrame {
 
 
         //ikona okna
-        ImageIcon icon = new ImageIcon("icon.png");
-        setIconImage(icon.getImage());
+        try {
+            ImageIcon icon = new ImageIcon(
+                    getClass().getResource("/resources/icon.png")
+
+            );
+            frame.setIconImage(icon.getImage());
+
+        }catch (Exception e){
+            System.out.println("icon obrazek nenalezen");
+        }
+
+
 
         //uvodni text
         JLabel title = new JLabel("PEXESO", SwingConstants.CENTER);
-        title.setFont(new Font("Arial", Font.BOLD, 32));
+        title.setFont(new Font("Arial", Font.BOLD, 40));
         frame.add(title, BorderLayout.NORTH);
 
 
         //obtiznosti
         JPanel diffs = new JPanel();
         diffs.setLayout((new BoxLayout(diffs, BoxLayout.X_AXIS)));
-        diffs.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
+        diffs.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
 
-        Dimension size = new Dimension(120, 40);
+        Dimension size = new Dimension(200, 300);
+
         JButton easy = new JButton("Easy 4x4");
         easy.setPreferredSize(size);
+        easy.setFocusPainted(false);  //smazani ramecku okolo textu
+        easy.setAlignmentX(CENTER_ALIGNMENT);
+
+
         JButton medium = new JButton("Medium 4x5");
         medium.setPreferredSize(size);
+        medium.setFocusPainted(false);  //smazani ramecku okolo textu
+        medium.setAlignmentX(CENTER_ALIGNMENT);
+
         JButton hard = new JButton("Hard 4x6");
         hard.setPreferredSize(size);
-        easy.setAlignmentX(CENTER_ALIGNMENT);
-        medium.setAlignmentX(CENTER_ALIGNMENT);
+        hard.setFocusPainted(false);  //smazani ramecku okolo textu
         hard.setAlignmentX(CENTER_ALIGNMENT);
+
+
         diffs.add(easy);
         diffs.add(Box.createHorizontalStrut(15));   //mezery
         diffs.add(medium);
